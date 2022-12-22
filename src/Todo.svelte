d6db75d5768a3d4a80b6148c6e8dd2734a27848a
TanguyLichau <tanguylichau@gmail.com>
2022-12-22 16:35:52 +0100

    Added todo and todolists components

---
+++ src/Todo.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let todoValue = "";
+  let todo = {
+    text: todoValue,
+    isChecked: false,
+  };
+
+  function checkTodo() {
+    todo.isChecked = !todo.isChecked;
+  }
+</script>
+
+<div>
+  {#if todo.isChecked}
+    <p class="checked">{todo.text}</p>
+  {:else}
+    <p class="notchecked">{todo.text}</p>
+  {/if}
+  <button on:click={checkTodo}> check</button>
+</div>
+
+<style>
+  .checked {
+    text-decoration: line-through;
+  }
+  p {
+    display: inline-block;
+    margin-right: 30px;
+  }
+  div {
+    text-align: center;
+  }
+</style>
---
+++ src/TodosList.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Todo from "./Todo.svelte";
+  export let todolists = [];
+</script>
+
+<ul>
+  {#each todolists as i}
+    <Todo todoValue={i} />
+  {/each}
+</ul>
