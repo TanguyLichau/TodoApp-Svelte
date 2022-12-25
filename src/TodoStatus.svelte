bcf0782cebd58d25ac6dd937bafecb85251cdcfe
TanguyLichau <tanguylichau@gmail.com>
2022-12-25 11:14:03 +0100

    Displaying different messages depending on number of todos

--- src/TodoStatus.svelte
+++ src/TodoStatus.svelte
@@ -1,11 +1,20 @@
 <script>
   export let todolists;
+
+  $: completedLength = todolists.filter(
+    (todo) => todo.isChecked === true
+  ).length;
 </script>
 
-<h2>
-  There are {todolists.filter((todo) => todo.isChecked === true).length} completed
-  todos out of {todolists.length}
-</h2>
+{#if todolists.length === 0}
+  <h2>You have no todos at the moment</h2>
+{:else if completedLength === todolists.length}
+  <h2>You have completed all of your current todos !</h2>
+{:else}
+  <h2>
+    There are {completedLength} completed todos out of {todolists.length}
+  </h2>
+{/if}
 
 <style>
   h2 {
